f704237920c4769cb1452188fb33434e2ae84ff3
Alex-Blv <83402296+Alex-Blv@users.noreply.github.com>
2022-09-05 21:45:16 +0300

    Delete Task_1 directory

--- Task_1/script.js
+++
@@ -1,8 +0,0 @@
-const btn = document.querySelector('.j-btn-test');
-const icon = document.querySelectorAll('.btn_icon');
-
-btn.addEventListener('click', () => {
-
-  icon.forEach((elem) => elem.classList.toggle('none'))
-  
-});
